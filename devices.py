6cca41e1b32f30532223f2c3d2195cbd9eb46eba
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-11-10 20:04:02 -0700

    Create devices.py

---
+++ devices.py
@@ -0,0 +1,17 @@
+# These IPs, names, and devices are all fake, but the concept is real.
+# I'm not sure exactly what I want to do yet with this, but I feel as
+# though it could be coming in handy in the future.
+
+ip_identifiers = {
+    "192.168.1.15": "Tony's iPhone",
+    "192.168.1.1": "Home Modem",
+    "192.168.1.13": "My MacBook Pro",
+    "192.168.1.51": "Tony's Dell Venue",
+    "192.168.1.46": "Living Room 4k AppleTV",
+    "192.168.1.4": "HomePod",
+    "192.168.1.3": "Sharon's iPod Touch",
+    "192.168.1.7": "Linda's Microsoft Surface",
+    "192.168.1.19": "My iPad Pro",
+    "192.168.1.18": "My PinePhone",
+
+}
